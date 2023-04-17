7548256e768a1b69ec26b769d0c58087360e7636
Roberto Losa <roberto.losa@lsvmultimedia.it>
2023-04-17 14:50:34 +0200

    Add images in home

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
+import Preloader from "./preloader-controller.js"
 
 export default class extends Controller {
   connect() {
     console.log("Main controller connected")
+
+    this.createPreloader()
+  }
+
+  createPreloader() {
+    this.preloader = new Preloader()
+    this.preloader.once("completed", this.onPreloaded.bind(this))
+  }
+
+  onPreloaded() {
+    this.preloader.destroy()
+    document.documentElement.classList.add("is-loaded")
   }
 }
